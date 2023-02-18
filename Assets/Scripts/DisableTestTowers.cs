f7a02f64d0dbfa37c8ff88522abbf8149a6b2f8e
SuhraabAlimi <70545301+SuhraabAlimi@users.noreply.github.com>
2023-02-18 00:01:58 -0600

    Added a UI element allowing you to select and deselect a tower to place on the map, working with 2 different towers at the moment.

---
+++ Assets/Scripts/DisableTestTowers.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableTestTowers : MonoBehaviour
+{
+    public GameObject cactus1;
+    public GameObject cactus2;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            if (cactus1.active)
+            {
+                cactus1.SetActive(false);
+            } else
+            {
+                cactus1.SetActive(true);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            if (cactus2.active)
+            {
+                cactus2.SetActive(false);
+            }
+            else
+            {
+                cactus2.SetActive(true);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ManuallySpawnEnemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManuallySpawnEnemy : MonoBehaviour
+{
+    public GameObject testEnemy;
+    public Transform spawnTransform;
+    public void spawnTestEnemy(int amount)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            Instantiate(testEnemy, spawnTransform.position, testEnemy.transform.rotation);
+        }
+    }
+}
