cf4a8b578edcf444e469397b13ecdaf7551ad2a4
Kevinthesun456 <55768043+Kevinthesun456@users.noreply.github.com>
2023-02-24 19:01:01 -0600

    Created working Pause functionality

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject menu;
+    public float timeSpeed;
+
+
+
+
+    void Start()
+    {
+        timeSpeed = Time.timeScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (menu.activeSelf)
+            {
+                menu.SetActive(false);
+                Time.timeScale = timeSpeed;
+
+            }
+            else
+            {
+                menu.SetActive(true);
+                timeSpeed = Time.timeScale;
+                Time.timeScale = 0f;
+
+            }
+
+        }
+
+
+
+    }
+}
