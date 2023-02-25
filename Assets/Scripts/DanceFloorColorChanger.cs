9cd599b9810b777802d5b1a5db14a17194e23ae6
SuhraabAlimi <70545301+SuhraabAlimi@users.noreply.github.com>
2023-02-24 18:29:13 -0600

    Made dance floor tiles change colors, and added and imported keyboard sounds to play during the procedural text

---
+++ Assets/Scripts/DanceFloorColorChanger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DanceFloorColorChanger : MonoBehaviour
+{
+    private MeshRenderer mr;
+    public float timer = 0.5f;
+    public float maxTime = 0.5f;
+    public Material[] floorMaterials;
+    void Start()
+    {
+        mr = gameObject.GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            timer = maxTime;
+            int ran = Random.Range(0, 7);
+            mr.material = floorMaterials[ran];
+        }
+    }
+}
