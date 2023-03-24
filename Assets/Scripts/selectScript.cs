63a338e9b44888aee7e18886a220d0f0a01bddb3
SuhraabAlimi <70545301+SuhraabAlimi@users.noreply.github.com>
2023-03-23 22:22:29 -0500

    Created full shop with working selectability and buttons for the cactus tower allowing upgrades to work

---
+++ Assets/Scripts/selectScript.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class selectScript : MonoBehaviour
+{
+	public GameObject range;
+	public GameObject UI;
+	public GameObject TopPath;
+	public GameObject BottomPath;
+	public Button button1;
+	public Button button2;
+	public int pricetop = 0;
+	public int pricebottom = 0;
+	public playerStats fortnite;
+	void Start()
+	{
+		GameObject[] ass = GameObject.FindGameObjectsWithTag("statistics");
+		fortnite = ass[0].GetComponent<playerStats>();
+	}
+	void Update()
+	{
+		int amountOfMoneyThatYouHaveAtThisMoment = playerStats.cash;
+		if (TopPath == null || amountOfMoneyThatYouHaveAtThisMoment < pricetop)
+		{
+			button1.interactable = false;
+		}
+		if (BottomPath == null || amountOfMoneyThatYouHaveAtThisMoment < pricebottom)
+		{
+			button2.interactable = false;
+		}
+
+		if (TopPath != null && amountOfMoneyThatYouHaveAtThisMoment >= pricetop)
+		{
+			button1.interactable = true;
+		}
+		if (BottomPath != null && amountOfMoneyThatYouHaveAtThisMoment >= pricebottom)
+		{
+			button2.interactable = true;
+		}
+	}
+
+	public void select()
+	{
+		range.SetActive(true);
+		StartCoroutine(selectThing());
+	}
+	public void deselect()
+	{
+		range.SetActive(false);
+		StartCoroutine(deselectThing());
+	}
+
+	public void upgrade()
+	{
+		Debug.Log("upgraded");
+		fortnite.addCash(-pricetop);
+		Instantiate(TopPath, transform.position, transform.rotation);
+		Destroy(gameObject);
+	}
+	public void upgrade2()
+	{
+		Debug.Log("upgraded");
+		fortnite.addCash(-pricebottom);
+		Instantiate(BottomPath, transform.position, transform.rotation);
+		Destroy(gameObject);
+	}
+	IEnumerator deselectThing()
+	{
+		yield return new WaitForSeconds(0.1f);
+		UI.SetActive(false);
+	}
+	IEnumerator selectThing()
+	{
+		yield return new WaitForSeconds(0.1f);
+		UI.SetActive(true);
+	}
+
+
+
+}
